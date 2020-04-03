828309f2875ddef1cf967a1936ae71155fa2feb0
Sinevia <info@sinevia.com>
2020-04-03 18:36:18 +0100

    Updates

--- main.php
+++ main.php
@@ -3,5 +3,6 @@
 function main()
 {
     require_once __DIR__ . '/app/functions.php';
+    \App\Helpers\Links::setBaseUrl(\Sinevia\Registry::get("URL_BASE", ""));
     return \App\Helpers\App::main();
 }
